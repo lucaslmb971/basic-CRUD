5fb4a5704fdd89240e58a26221127af404f5e126
lucaslmb971 <lucaslmb971@gmail.com>
2024-06-19 17:01:50 -0400

    commit mac

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,4 +1,13 @@
 const { Router } = require('express')
 const router = Router()
 
+router.route('/')
+    .get((req, res) => res.send('user routes'))
+    .post()
+
+router.route('/:id')
+    .put()
+    .delete()
+
+
 module.exports = router;
